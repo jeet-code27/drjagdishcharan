e8072dd5a1a3c0d9020d75410c204648c2c60649
Abhay-wdev <abhaychorsiya.dev@gmail.com>
2025-06-30 18:25:56 +0530

    version4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -71,7 +71,7 @@ export default function Navbar() {
   const isActive = (href) => pathname === href;
 
   return (
-    <nav ref={navRef} className="bg-white border-b border-gray-200 z-50 w-full">
+    <nav ref={navRef} className="bg-white border-b border-gray-200 z-50 w-screen">
       <div className="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between px-4 py-3">
         {/* Logo */}
         <Link href="/" className="flex items-center space-x-3">
@@ -202,9 +202,19 @@ export default function Navbar() {
             </li>
 
             <li className="w-full md:w-auto">
-              <button className="w-full md:w-auto bg-indigo-600 text-white font-semibold text-sm px-4 py-2 rounded-lg shadow-md transition hover:bg-indigo-700 hover:-translate-y-0.5 hover:shadow-lg active:translate-y-0">
-                Book Appointment
-              </button>
+            
+              
+<button className="group cursor-pointer relative inline-flex h-[calc(30px+8px)] items-center justify-center rounded-full bg-blue-600 pl-3 pr-14 font-medium text-neutral-50">
+  <span className="z-10 pr-2 font-bold">Appointment</span>
+  <div className="absolute right-1 inline-flex h-8 w-10 items-center justify-end rounded-full bg-[#003153] transition-[width] group-hover:w-[calc(100%-8px)]">
+    <div className="mr-3.5 flex items-center justify-center">
+      <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-neutral-50">
+        <path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+      </svg>
+    </div>
+  </div>
+</button>
+
             </li>
           </ul>
         </div>
